f9523594eee0c10c9fd3f73bb1082129568aebcd
Brent Dacus <22330436+bdacus01@users.noreply.github.com>
2019-07-01 15:52:44 -0500

    updated named.conf

--- da-slave-install.sh
+++ da-slave-install.sh
@@ -42,7 +42,7 @@ chown named:named -R /usr/local/directslave
 cat > /usr/local/directslave/etc/directslave.conf <<EOF
 background	1
 
-host            *
+host            "$3"
 
 port            2222
 ssl             off
@@ -80,7 +80,7 @@ mkdir /var/log/named
 touch /var/log/named/security.log
 chmod a+w -R /var/log/named
 
-cat > /etc/named.comf <<EOF
+cat > /etc/named.conf <<EOF
 //
 // named.conf
 //
@@ -89,53 +89,62 @@ cat > /etc/named.comf <<EOF
 //
 // See /usr/share/doc/bind*/sample/ for example named configuration files.
 //
+// See the BIND Administrator's Reference Manual (ARM) for details about the
+// configuration located in /usr/share/doc/bind-{version}/Bv9ARM.html
 
 options {
-        listen-on port 53 { any; };
-        listen-on-v6 port 53 { any; };
-        directory       \"/var/named\";
-        dump-file       \"/var/named/data/cache_dump.db\";
-        statistics-file \"/var/named/data/named_stats.txt\";
-        memstatistics-file \"/var/named/data/named_mem_stats.txt\";
-        allow-query     { any; };
-        allow-recursion { none; };
+	listen-on port 53 { any; };
+	listen-on-v6 port 53 { none; };
+	directory 	"/var/named";
+	dump-file 	"/var/named/data/cache_dump.db";
+	statistics-file "/var/named/data/named_stats.txt";
+	memstatistics-file "/var/named/data/named_mem_stats.txt";
+	recursing-file  "/var/named/data/named.recursing";
+	secroots-file   "/var/named/data/named.secroots";
+		allow-query     { any; };
+		allow-recursion { none; };
 		allow-notify	{ "$3"; };
 		allow-transfer	{ none; };
-        dnssec-enable yes;
-        dnssec-validation yes;
 
-        /* Path to ISC DLV key */
-        bindkeys-file \"/etc/named.iscdlv.key\";
+	/*
+	 - If you are building an AUTHORITATIVE DNS server, do NOT enable recursion.
+	 - If you are building a RECURSIVE (caching) DNS server, you need to enable
+	   recursion.
+	 - If your recursive DNS server has a public IP address, you MUST enable access
+	   control to limit queries to your legitimate users. Failing to do so will
+	   cause your server to become part of large scale DNS amplification
+	   attacks. Implementing BCP38 within your network would greatly
+	   reduce such attack surface
+	*/
+	recursion no;
 
-        managed-keys-directory \"/var/named/dynamic\";
+	dnssec-enable yes;
+	dnssec-validation yes;
+
+	/* Path to ISC DLV key */
+	bindkeys-file "/etc/named.iscdlv.key";
+
+	managed-keys-directory "/var/named/dynamic";
+
+	pid-file "/run/named/named.pid";
+	session-keyfile "/run/named/session.key";
 };
 
-
-
 logging {
-		channel security_file {
-			file \"/var/log/named/security.log\" versions 3 size 30m;
-			severity dynamic;
-			print-time yes;
-		};
-		category security {
-			security_file;
-		};
         channel default_debug {
-                file \"data/named.run\";
+                file "data/named.run";
                 severity dynamic;
         };
 };
 
-zone \".\" IN {
-        type hint;
-        file \"named.ca\";
+zone "." IN {
+	type hint;
+	file "named.ca";
 };
 
-include \"/etc/named.rfc1912.zones\";
-include \"/etc/named.root.key\";
-
-include \"/etc/namedb/directslave.inc\";
+include "/etc/named.rfc1912.zones";
+include "/etc/named.root.key";
+include "/etc/namedb/directslave.inc";
 EOF
 
 /usr/local/directslave/bin/directslave --password $1:$2
